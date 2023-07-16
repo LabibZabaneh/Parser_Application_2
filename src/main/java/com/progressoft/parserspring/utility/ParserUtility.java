86b77b363775cc68471414d8f317d19b738d24de
Labib <ps.Labib.Zabaneh@progressoft.io>
2023-07-16 16:09:37 +0300

    Added the parser Utility

---
+++ src/main/java/com/progressoft/parserspring/utility/ParserUtility.java
@@ -0,0 +1,35 @@
+package com.progressoft.parserspring.utility;
+
+import com.progressoft.interns.advanced.parser.CsvParserImpl;
+import com.progressoft.interns.advanced.parser.Parser;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+
+public class ParserUtility {
+    public static ArrayList<Object[]> parseData(MultipartFile file){
+        ArrayList<Object[]> data = null;
+        try {
+            Path csvToParse = Files.createTempFile("csvToParse", ".csv");
+            String fileContent = new String(file.getBytes(), StandardCharsets.UTF_8);
+            Files.write(csvToParse, fileContent.getBytes(), StandardOpenOption.WRITE);
+            Parser<ArrayList<Object[]>> parser = new CsvParserImpl();
+            data = parser.parse(csvToParse.toString());
+
+            for (Object[] row : data) {
+                for (Object cell : row) {
+                    System.out.print(cell + " ");
+                }
+                System.out.println();
+            }
+        } catch (IOException e){
+            System.out.println("Error in parsing the data");
+        }
+        return data;
+    }
+}
